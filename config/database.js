4cd7820edcb632f9f3d3f341adeec788d844a100
Shiny <it.avalanchedev@gmail.com>
2024-10-20 23:37:46 +0500

    helo

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const db_Connect = () => {
-    mongoose.connect(process.env.MONGODB_URL,{}).then((conn) => {
+    mongoose.connect(process.env.MONGODB_URL).then((conn) => {
         console.log(`DB Connected Successfully`);
         
     }).catch((err) => {
